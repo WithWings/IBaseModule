0f3f20feec2d7f7283f30855ca039764acacad4a
lamian <lamian@2dfire.com>
2017-11-01 10:19:00 +0800

    productFlavors 引用不同代码

---
+++ app/src/wandoujia/java/ll/channel/diff/ToastDiff.java
@@ -0,0 +1,17 @@
+package ll.channel.diff;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * 测试 productFlavors
+ * 创建：WithWings 时间：2017/11/1.
+ * Email:wangtong1175@sina.com
+ */
+public class ToastDiff {
+
+    public static void showFrom(Context context){
+        Toast.makeText(context, "豌豆荚", Toast.LENGTH_SHORT).show();
+    }
+
+}
