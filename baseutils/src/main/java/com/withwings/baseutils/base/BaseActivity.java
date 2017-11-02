85056d5466e40309c9422d526de4cb5715989fe1
wangtong <wangtong@test.com>
2017-11-02 17:01:04 +0800

    base 基类

---
+++ baseutils/src/main/java/com/withwings/baseutils/base/BaseActivity.java
@@ -0,0 +1,92 @@
+package com.withwings.baseutils.base;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * 基本 Activity 封装
+ * 创建：WithWings 时间：2017/10/25.
+ * Email:wangtong1175@sina.com
+ */
+public abstract class BaseActivity extends Activity {
+
+    protected Activity mActivity;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mActivity = this;
+
+        setContentView(getLayoutId());
+
+        initView();
+
+        initData();
+
+        initListener();
+    }
+
+    protected abstract int getLayoutId();
+
+    protected abstract void initView();
+
+    protected abstract void initData();
+
+    protected abstract void initListener();
+
+    /**
+     * 打开某个界面 singleTask 效果
+     * @param activityClass 界面
+     */
+    protected void startActivityForOnlyOne(Class<? extends Activity> activityClass){
+        startActivityForOnlyOne(activityClass,0,false);
+    }
+
+    /**
+     * 打开某个界面 singleTask 效果 获得打开界面返回值
+     * @param activityClass 界面
+     * @param requestCode 请求标记
+     */
+    protected void startActivityForOnlyOne(Class<? extends Activity> activityClass,int requestCode){
+        startActivityForOnlyOne(activityClass,requestCode,true);
+    }
+
+    private void startActivityForOnlyOne(Class<? extends Activity> activityClass,int requestCode,boolean forResult){
+        Intent intent = new Intent(mActivity,activityClass);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        if(forResult) {
+            startActivityForResult(intent,requestCode);
+        } else {
+            startActivity(intent);
+        }
+    }
+
+    /**
+     * 打开一个界面
+     * @param activityClass 界面
+     */
+    protected void startActivity(Class<? extends Activity> activityClass) {
+        startActivity(activityClass,0,false);
+    }
+
+    /**
+     * 打开一个界面 获得打开界面返回值
+     * @param activityClass 界面
+     * @param requestCode 请求标记
+     */
+    protected void startActivityForResult(Class<? extends Activity> activityClass,int requestCode) {
+        startActivity(activityClass,requestCode,true);
+    }
+
+    private void startActivity(Class<? extends Activity> activityClass,int requestCode,boolean forResult){
+        Intent intent = new Intent(mActivity,activityClass);
+        if(forResult) {
+            startActivityForResult(intent,requestCode);
+        } else {
+            startActivity(intent);
+        }
+    }
+}
---
+++ baseutils/src/main/java/com/withwings/baseutils/base/BaseApplication.java
@@ -0,0 +1,17 @@
+package com.withwings.baseutils.base;
+
+import android.app.Application;
+
+/**
+ * 基础类 Application 初始化一些数据
+ * 创建：WithWings 时间：2017/10/25.
+ * Email:wangtong1175@sina.com
+ */
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
