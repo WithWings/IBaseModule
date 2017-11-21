164c0cdee013030be0f09fb66d4542476b003e45
wangtong <wangtong@test.com>
2017-11-21 13:40:51 +0800

    优化座机键盘

---
+++ baseutils/src/main/java/com/withwings/baseutils/utils/BitmapUtils.java
@@ -0,0 +1,156 @@
+package com.withwings.baseutils.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+
+/**
+ * 图片处理工具
+ * 创建：WithWings 时间：2017/11/20.
+ * Email:wangtong1175@sina.com
+ */
+public class BitmapUtils {
+
+    /**
+     * 矩形将图片的四角圆化
+     * @param bitmap 传入Bitmap对象
+     * @return bitmap 对象
+     */
+    public static Bitmap getRoundedCornerBitmap(Bitmap bitmap) {
+        return getRoundedCornerBitmap(bitmap,50);
+    }
+
+    /**
+     * 矩形将图片的四角圆化
+     * @param bitmap 传入Bitmap对象
+     * @param roundPx 圆角度数
+     * @return bitmap 对象
+     */
+    public static Bitmap getRoundedCornerBitmap(Bitmap bitmap,float roundPx) {
+        if(bitmap == null) {
+            return null;
+        }
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        // 得到画布
+        Canvas canvas = new Canvas(output);
+        // 将画布的四角圆化
+        final int color = Color.RED;
+        final Paint paint = new Paint();
+        // 得到与图像相同大小的区域 由构造的四个值决定区域的位置以及大小
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+        // 值越大角度越明显
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        // drawRoundRect的第2,3个参数一样则画的是正圆的一角，如果数值不同则是椭圆的一角
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
+    }
+
+    /**
+     * 矩形将图片圆化
+     *
+     * @param bitmap 传入Bitmap对象
+     * @return bitmap 对象
+     */
+    public static Bitmap toRoundBitmap(Bitmap bitmap) {
+        if(bitmap == null) {
+            return null;
+        }
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        float roundPx;
+        float left, top, right, bottom, dst_left, dst_top, dst_right, dst_bottom;
+        if (width <= height) {
+            roundPx = width / 2;
+            left = 0;
+            top = 0;
+            right = width;
+            bottom = width;
+            height = width;
+            dst_left = 0;
+            dst_top = 0;
+            dst_right = width;
+            dst_bottom = width;
+        } else {
+            roundPx = height / 2;
+            float clip = (width - height) / 2;
+            left = clip;
+            right = width - clip;
+            top = 0;
+            bottom = height;
+            width = height;
+            dst_left = 0;
+            dst_top = 0;
+            dst_right = height;
+            dst_bottom = height;
+        }
+
+        Bitmap output = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect src = new Rect((int) left, (int) top, (int) right,
+                (int) bottom);
+        final Rect dst = new Rect((int) dst_left, (int) dst_top,
+                (int) dst_right, (int) dst_bottom);
+        final RectF rectF = new RectF(dst);
+
+        paint.setAntiAlias(true);// 设置画笔无锯齿
+
+        canvas.drawARGB(0, 0, 0, 0); // 填充整个Canvas
+        paint.setColor(color);
+
+        // 以下有两种方法画圆,drawRounRect和drawCircle
+        // canvas.drawRoundRect(rectF, roundPx, roundPx, paint);//
+        // 画圆角矩形，第一个参数为图形显示区域，第二个参数和第三个参数分别是水平圆角半径和垂直圆角半径。
+        canvas.drawCircle(roundPx, roundPx, roundPx, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));// 设置两张图片相交时的模式,参考http://trylovecatch.iteye.com/blog/1189452
+        canvas.drawBitmap(bitmap, src, dst, paint); // 以Mode.SRC_IN模式合并bitmap和已经draw了的Circle
+
+        return output;
+    }
+
+    /**
+     * Drawable 转 Bitmap
+     * @param drawable 数据
+     * @return 结果
+     */
+    public static Bitmap drawableToBitmap(Drawable drawable) {
+
+        Bitmap bitmap = Bitmap.createBitmap(
+                drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight(),
+                drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888
+                        : Bitmap.Config.RGB_565);
+        Canvas canvas = new Canvas(bitmap);
+        //canvas.setBitmap(bitmap);
+        drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+        drawable.draw(canvas);
+        return bitmap;
+    }
+
+    /**
+     * Bitmap 转 Drawable
+     * @param bitmap 数据
+     * @return 结果
+     */
+    public static Drawable bitmapToDrawable(Bitmap bitmap) {
+        return new BitmapDrawable(bitmap);
+    }
+
+}
