0e2d70f86696768719ec947eb5333a53cad83458
wangtong <wangtong@test.com>
2017-11-23 17:48:55 +0800

    权限工具类国内机兼容性修改

--- baselibs/src/main/java/com/withwings/baselibs/andpermission/PermissionUtils.java
+++ baselibs/src/main/java/com/withwings/baselibs/andpermission/PermissionUtils.java
@@ -23,6 +23,16 @@ public class PermissionUtils {
 
     }
 
+    /**
+     * 建议用 mustPermission 在部分手机上，用户第一次拒绝后就相当于勾选了不再提示
+     * 所以除非是需求度不高的权限，否则还是建议还是用mustPermission
+     *
+     * @param context               上下文对象
+     * @param requestCode           请求 code
+     * @param rationaleDialog       用户拒绝后的提示框
+     * @param andPermissionListener 权限授予情况的监听器
+     * @param permissions           申请的权限表
+     */
     public static void checkPermission(final Context context, int requestCode, SettingDialogShow rationaleDialog, final AndPermissionListener andPermissionListener, String... permissions) {
         // 敲黑板，Arrays.asList 返回的集合不可修改
         final List<String> requestList = Arrays.asList(permissions);
@@ -34,18 +44,51 @@ public class PermissionUtils {
                     @Override
                     public void onSucceed(int requestCode, @NonNull List<String> grantPermissions) {
                         if (andPermissionListener != null) {
-                            andPermissionListener.onAllSucceed(requestCode, grantPermissions);
-                            andPermissionListener.onSucceed(requestCode, grantPermissions);
+                            List<String> copyGrantPermissions = new ArrayList<>();
+                            copyGrantPermissions.addAll(grantPermissions);
+                            for (String copyGrantPermission : copyGrantPermissions) {
+                                if (!AndPermission.hasPermission(context, copyGrantPermission)) {
+                                    grantPermissions.remove(copyGrantPermission);
+                                }
+                            }
+                            if (grantPermissions.size() == requestList.size()) {
+                                andPermissionListener.onAllSucceed(requestCode, grantPermissions);
+                                andPermissionListener.onSucceed(requestCode, grantPermissions);
+                            } else if (grantPermissions.size() == 0) {
+                                andPermissionListener.onAllFailed(requestCode, grantPermissions);
+                                andPermissionListener.onFailed(requestCode, grantPermissions);
+                            } else {
+                                andPermissionListener.onSucceed(requestCode, grantPermissions);
+
+                                List<String> deniedPermissions = new ArrayList<>();
+                                deniedPermissions.addAll(requestList);
+                                // 部分成功
+                                if (deniedPermissions.removeAll(grantPermissions)) {
+                                    andPermissionListener.onFailed(requestCode, deniedPermissions);
+                                }
+                            }
                         }
                     }
 
                     @Override
                     public void onFailed(int requestCode, @NonNull List<String> deniedPermissions) {
                         if (andPermissionListener != null) {
+                            // 移除某些通知没有获得，实际已经获得的权限
+                            List<String> copyDeniedPermissions = new ArrayList<>();
+                            copyDeniedPermissions.addAll(deniedPermissions);
+                            for (String deniedPermission : copyDeniedPermissions) {
+                                if (AndPermission.hasPermission(context, deniedPermission)) {
+                                    deniedPermissions.remove(deniedPermission);
+                                }
+                            }
+
                             // 全部失败
                             if (deniedPermissions.size() == requestList.size()) {
                                 andPermissionListener.onAllFailed(requestCode, deniedPermissions);
                                 andPermissionListener.onFailed(requestCode, deniedPermissions);
+                            } else if (deniedPermissions.size() == 0) {
+                                andPermissionListener.onAllSucceed(requestCode, requestList);
+                                andPermissionListener.onSucceed(requestCode, requestList);
                             } else {
                                 // 部分失败
                                 andPermissionListener.onFailed(requestCode, deniedPermissions);
@@ -62,6 +105,16 @@ public class PermissionUtils {
                 .start();
     }
 
+    /**
+     * 用户勾选后仍然可以弹窗让用户去设置界面设置
+     *
+     * @param activity              跳转设置界面依赖的界面
+     * @param requestCode           请求code
+     * @param rationaleDialog       说明权限用处的弹窗
+     * @param settingDialog         跳转设置的弹窗
+     * @param andPermissionListener 权限授予情况的监听器
+     * @param permissions           申请的权限表
+     */
     public static void mustPermission(final Activity activity, int requestCode, final SettingDialogShow rationaleDialog, final SettingDialogShow settingDialog, final AndPermissionListener andPermissionListener, String... permissions) {
         // 敲黑板，Arrays.asList 返回的集合不可修改
         final List<String> requestList = Arrays.asList(permissions);
@@ -73,18 +126,51 @@ public class PermissionUtils {
                     @Override
                     public void onSucceed(int requestCode, @NonNull List<String> grantPermissions) {
                         if (andPermissionListener != null) {
-                            andPermissionListener.onAllSucceed(requestCode, grantPermissions);
-                            andPermissionListener.onSucceed(requestCode, grantPermissions);
+                            List<String> copyGrantPermissions = new ArrayList<>();
+                            copyGrantPermissions.addAll(grantPermissions);
+                            for (String copyGrantPermission : copyGrantPermissions) {
+                                if (!AndPermission.hasPermission(activity, copyGrantPermission)) {
+                                    grantPermissions.remove(copyGrantPermission);
+                                }
+                            }
+                            if (grantPermissions.size() == requestList.size()) {
+                                andPermissionListener.onAllSucceed(requestCode, grantPermissions);
+                                andPermissionListener.onSucceed(requestCode, grantPermissions);
+                            } else if (grantPermissions.size() == 0) {
+                                andPermissionListener.onAllFailed(requestCode, grantPermissions);
+                                andPermissionListener.onFailed(requestCode, grantPermissions);
+                            } else {
+                                andPermissionListener.onSucceed(requestCode, grantPermissions);
+
+                                List<String> deniedPermissions = new ArrayList<>();
+                                deniedPermissions.addAll(requestList);
+                                // 部分成功
+                                if (deniedPermissions.removeAll(grantPermissions)) {
+                                    andPermissionListener.onFailed(requestCode, deniedPermissions);
+                                }
+                            }
                         }
                     }
 
                     @Override
                     public void onFailed(int requestCode, @NonNull List<String> deniedPermissions) {
                         if (andPermissionListener != null) {
+                            // 移除某些通知没有获得，实际已经获得的权限
+                            List<String> copyDeniedPermissions = new ArrayList<>();
+                            copyDeniedPermissions.addAll(deniedPermissions);
+                            for (String deniedPermission : copyDeniedPermissions) {
+                                if (AndPermission.hasPermission(activity, deniedPermission)) {
+                                    deniedPermissions.remove(deniedPermission);
+                                }
+                            }
+
                             // 全部失败
                             if (deniedPermissions.size() == requestList.size()) {
                                 andPermissionListener.onAllFailed(requestCode, deniedPermissions);
                                 andPermissionListener.onFailed(requestCode, deniedPermissions);
+                            } else if (deniedPermissions.size() == 0) {
+                                andPermissionListener.onAllSucceed(requestCode, requestList);
+                                andPermissionListener.onSucceed(requestCode, requestList);
                             } else {
                                 // 部分失败
                                 andPermissionListener.onFailed(requestCode, deniedPermissions);
@@ -95,12 +181,11 @@ public class PermissionUtils {
                                     andPermissionListener.onSucceed(requestCode, grantPermissions);
                                 }
                             }
+                        }
 
-
-                            // 是否有不再提示并拒绝的权限。
-                            if (settingDialog != null && AndPermission.hasAlwaysDeniedPermission(activity, deniedPermissions)) {
-                                settingDialog.show();
-                            }
+                        // 是否有不再提示并拒绝的权限。
+                        if (settingDialog != null && AndPermission.hasAlwaysDeniedPermission(activity, deniedPermissions)) {
+                            settingDialog.show();
                         }
                     }
                 })
