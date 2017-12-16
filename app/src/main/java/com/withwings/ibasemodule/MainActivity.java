6197c155df930e964a6030d95b3c598c1f131e14
王彤 <wangtong@sinafenqi.com>
2017-12-16 17:33:32 +0800

    删除无用测试逻辑

--- app/src/main/java/com/withwings/ibasemodule/MainActivity.java
+++ app/src/main/java/com/withwings/ibasemodule/MainActivity.java
@@ -1,14 +1,9 @@
 package com.withwings.ibasemodule;
 
-import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.withwings.baseutils.base.BaseActivity;
-import com.withwings.basewidgets.image.newest.bean.NewestMessage;
-import com.withwings.basewidgets.image.newest.listener.OnNewestPopupClickListener;
-import com.withwings.basewidgets.image.newest.view.NewestPopupWindow;
 
 public class MainActivity extends BaseActivity {
 
@@ -43,12 +38,7 @@ public class MainActivity extends BaseActivity {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_executive_operation:
-                new NewestPopupWindow(mActivity).listener(new OnNewestPopupClickListener() {
-                    @Override
-                    public void onClick(View view, NewestMessage newestMessage) {
-                        Toast.makeText(mActivity, newestMessage.getName(), Toast.LENGTH_SHORT).show();
-                    }
-                }).size(TypedValue.COMPLEX_UNIT_DIP, 120, 120).show(v);
+
                 break;
         }
     }
