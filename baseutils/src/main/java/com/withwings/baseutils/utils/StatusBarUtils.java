ae3ce3141716e8642bb3b5d79f20dbe848c1bec9
王彤 <wangtong@sinafenqi.com>
2018-04-17 17:21:12 +0800

    状态栏适配 8.0

--- baseutils/src/main/java/com/withwings/baseutils/utils/StatusBarUtils.java
+++ baseutils/src/main/java/com/withwings/baseutils/utils/StatusBarUtils.java
@@ -1,6 +1,8 @@
 package com.withwings.baseutils.utils;
 
+import android.annotation.TargetApi;
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -14,135 +16,164 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.PixelFormat;
 import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.DisplayMetrics;
 
 /**
- * 状态栏工具类
+ * 状态栏工具类:已适配 8.0 通知栏 如果使用请确保 targetSdkVersion 已经指定到了26或者更高
  * 创建：WithWings 时间 2018/2/24
  * Email:wangtong1175@sina.com
  */
-@SuppressWarnings({"unused", "DanglingJavadoc"})
+@SuppressWarnings({"unused", "DanglingJavadoc", "WeakerAccess", "SameParameterValue"})
 public class StatusBarUtils {
 
     /**
-     * 显示一个普通的通知
+     * 单个通知渠道注册：重复id会覆盖，新的 id 会新增，上次注册本次不注册并不会删除，但是如果是新用户则仍然没有该渠道，发送消息会 Toast 异常。
      *
-     * @param context 上下文
+     * @param context     上下文
+     * @param channelId   渠道标记
+     * @param channelName 渠道名
+     * @param importance  渠道权限 ：
+     *                    NotificationManager.IMPORTANCE_DEFAULT ：发出提示音显示在状态栏
+     *                    NotificationManager.IMPORTANCE_NONE ：关闭，即使用户手动打开也仅相当于 IMPORTANCE_LOW
+     *                    NotificationManager.IMPORTANCE_MIN ：仅发出提示音：比如创建界面角标的时候使用
+     *                    NotificationManager.IMPORTANCE_LOW ：显示在状态栏
+     *                    NotificationManager.IMPORTANCE_HIGH ：发出提示音显示在状态栏并在屏幕上弹出通知
+     *                    NotificationManager.IMPORTANCE_UNSPECIFIED ：未指定，无法使用
      */
-    public static void showNotification(Context context) {
-        Notification notification = new NotificationCompat.Builder(context)
-                /**设置通知左边的大图标**/
-                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), getIcon(context.getPackageName(), context)))
-                /**设置通知右边的小图标**/
-                .setSmallIcon(getIcon(context.getPackageName(), context))
-                /**通知首次出现在通知栏，带上升动画效果的**/
-                .setTicker("通知来了")
-                /**设置通知的标题**/
-                .setContentTitle("这是一个通知的标题")
-                /**设置通知的内容**/
-                .setContentText("这是一个通知的内容这是一个通知的内容")
-                /**通知产生的时间，会在通知信息里显示**/
-                .setWhen(System.currentTimeMillis())
-                /**设置该通知优先级**/
-                .setPriority(Notification.PRIORITY_DEFAULT)
-                /**设置这个标志当用户单击面板就可以让通知将自动取消**/
-                .setAutoCancel(true)
-                /**设置他为一个正在进行的通知。他们通常是用来表示一个后台任务,用户积极参与(如播放音乐)或以某种方式正在等待,因此占用设备(如一个文件下载,同步操作,主动网络连接)**/
-                .setOngoing(false)
-                /**向通知添加声音、闪灯和振动效果的最简单、最一致的方式是使用当前的用户默认设置，使用defaults属性，可以组合：**/
-                .setDefaults(Notification.DEFAULT_VIBRATE | Notification.DEFAULT_SOUND)
-                // TODO 点击效果
-//                .setContentIntent(PendingIntent.getActivity(context, 1, new Intent(context, MainActivity.class), PendingIntent.FLAG_CANCEL_CURRENT))
-                .build();
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
-        /**发起通知**/
-        notificationManager.notify(0, notification);
-    }
-
-    /**
-     * 显示一个下载带进度条的通知
-     *
-     * @param context 上下文
-     */
-    public static NotificationCompat.Builder showNotificationProgress(Context context) {
-        //进度条通知
-        final NotificationCompat.Builder builderProgress = new NotificationCompat.Builder(context);
-        builderProgress.setContentTitle("下载中");
-        builderProgress.setSmallIcon(getIcon(context.getPackageName(), context));
-        builderProgress.setTicker("进度条通知");
-        builderProgress.setProgress(100, 0, false);
-        final Notification notification = builderProgress.build();
-        final NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        //发送一个通知
-        if (notificationManager != null) {
-            notificationManager.notify(2, notification);
+    @TargetApi(Build.VERSION_CODES.O)
+    public static void createNotificationChannel(Context context, String channelId, String channelName, int importance) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId, channelName, importance);
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(channel);
+            }
         }
+    }
 
-//        builderProgress.setProgress(100, progress, false);
-//        builderProgress.cancel(2);
-        return builderProgress;
+    /**
+     * 删除渠道，但是注意，这里是会给老用户展示删除记录的。
+     *
+     * @param context   上下文
+     * @param channelId 渠道Id
+     */
+    @TargetApi(Build.VERSION_CODES.O)
+    public static void deleteNotificationChannel(Context context, String channelId) {
+        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (manager != null) {
+            manager.deleteNotificationChannel(channelId);
+        }
+    }
+
+    /**
+     * 基本弹框，无法点击
+     *
+     * @param context   上下文
+     * @param channelId 渠道id
+     * @param title     标题
+     * @param text      文字
+     */
+    public static void sendMessage(Context context, String channelId, String title, String text) {
+        sendMessage(context, channelId, title, text, 0, null);
+    }
+
+    /**
+     * 基本弹框，无法点击，带有数字
+     *
+     * @param context   上下文
+     * @param channelId 渠道id
+     * @param title     标题
+     * @param text      文字
+     * @param number    消息数
+     */
+    public static void sendMessage(Context context, String channelId, String title, String text, int number) {
+        sendMessage(context, channelId, title, text, number, null);
+    }
+
+    /**
+     * 基本弹框，可以点击，不带有数字
+     *
+     * @param context       上下文
+     * @param channelId     渠道id
+     * @param title         标题
+     * @param text          文字
+     * @param pendingIntent 点击动作
+     */
+    public static void sendMessage(Context context, String channelId, String title, String text, PendingIntent pendingIntent) {
+        sendMessage(context, channelId, title, text, 0, pendingIntent);
+    }
+
+    /**
+     * 基本弹框，可以点击，不带有数字
+     *
+     * @param context   上下文
+     * @param channelId 渠道id
+     * @param title     标题
+     * @param text      文字
+     * @param intent    点击动作未封装
+     */
+    public static void sendMessage(Context context, String channelId, String title, String text, Intent intent) {
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        sendMessage(context, channelId, title, text, 0, pendingIntent);
+    }
+
+    /**
+     * 发送消息
+     *
+     * @param context       上下文
+     * @param channelId     渠道id
+     * @param title         标题
+     * @param text          文字
+     * @param number        消息数
+     * @param pendingIntent 点击动作
+     */
+    public static void sendMessage(Context context, String channelId, String title, String text, int number, PendingIntent pendingIntent) {
+        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelId).setContentTitle(title).setContentText(text).setWhen(System.currentTimeMillis()).setSmallIcon(getIcon(context)).setLargeIcon(BitmapFactory.decodeResource(context.getResources(), getIcon(context))).setAutoCancel(true);
+        if (number > 0) {
+            builder.setNumber(number);
+        }
+        if (pendingIntent != null) {
+            builder.setFullScreenIntent(pendingIntent, true);
+        }
+        Notification notification = builder.build();
+        if (manager != null) {
+            manager.notify(1, notification);
+        }
     }
 
 
     /**
-     * 悬挂式,部分系统厂商不支持
+     * 获得当前APP ICON
      *
-     * @param context
+     * @param context 上下文
+     * @return 图标
      */
-    public static void showFullScreen(Context context) {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
-        Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://blog.csdn.net/linglongxin24"));
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, mIntent, 0);
-        builder.setContentIntent(pendingIntent);
-        builder.setSmallIcon(getIcon(context.getPackageName(), context));
-        builder.setLargeIcon(BitmapFactory.decodeResource(context.getResources(),getIcon(context.getPackageName(), context)));
-        builder.setAutoCancel(true);
-        builder.setContentTitle("悬挂式通知");
-        // TODO 设置点击跳转
-//        Intent hangIntent = new Intent();
-//        hangIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//        hangIntent.setClass(context, MainActivity.class);
-//        //如果描述的PendingIntent已经存在，则在产生新的Intent之前会先取消掉当前的
-//        PendingIntent hangPendingIntent = PendingIntent.getActivity(context, 0, hangIntent, PendingIntent.FLAG_CANCEL_CURRENT);
-//        builder.setFullScreenIntent(hangPendingIntent, true);
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
-        notificationManager.notify(3, builder.build());
+    public static Drawable getIconFromPackageName(Context context) {
+        return getIconFromPackageName(context.getPackageName(), context);
     }
 
     /**
-     * 折叠式
+     * 获得指定包名 ICON
      *
-     * @param context
+     * @param packageName 包名
+     * @param context     上下文
+     * @return 图标
      */
-//    public static void shwoNotify(Context context) {
-//        //先设定RemoteViews
-//        RemoteViews view_custom = new RemoteViews(context.getPackageName(), R.layout.view_custom);
-//        //设置对应IMAGEVIEW的ID的资源图片
-//        view_custom.setImageViewResource(R.id.custom_icon, getIcon(context.getPackageName(), context));
-//        view_custom.setTextViewText(R.id.tv_custom_title, "今日头条");
-//        view_custom.setTextColor(R.id.tv_custom_title, Color.BLACK);
-//        view_custom.setTextViewText(R.id.tv_custom_content, "金州勇士官方宣布球队已经解雇了主帅马克-杰克逊，随后宣布了最后的结果。");
-//        view_custom.setTextColor(R.id.tv_custom_content, Color.BLACK);
-//        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context);
-//        mBuilder.setContent(view_custom).setContentIntent(PendingIntent.getActivity(context, 4, new Intent(context, MainActivity.class), PendingIntent.FLAG_CANCEL_CURRENT)).setWhen(System.currentTimeMillis())// 通知产生的时间，会在通知信息里显示
-//                .setTicker("有新资讯").setPriority(Notification.PRIORITY_HIGH)// 设置该通知优先级
-//                .setOngoing(false)//不是正在进行的   true为正在进行  效果和.flag一样
-//                .setSmallIcon(getIcon(context.getPackageName(), context));
-//        Notification notify = mBuilder.build();
-//        NotificationManager notificationManager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
-//        notificationManager.notify(4, notify);
-//    }
-
     public static Drawable getIconFromPackageName(String packageName, Context context) {
         PackageManager pm = context.getPackageManager();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
             try {
                 PackageInfo pi = pm.getPackageInfo(packageName, 0);
                 Context otherAppCtx = context.createPackageContext(packageName, Context.CONTEXT_IGNORE_SECURITY);
-                int displayMetrics[] = {DisplayMetrics.DENSITY_XXXHIGH, DisplayMetrics.DENSITY_XXHIGH, DisplayMetrics.DENSITY_XHIGH, DisplayMetrics.DENSITY_HIGH, DisplayMetrics.DENSITY_TV};
+                int[] displayMetrics;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                    displayMetrics = new int[]{DisplayMetrics.DENSITY_XXXHIGH, DisplayMetrics.DENSITY_XXHIGH, DisplayMetrics.DENSITY_XHIGH, DisplayMetrics.DENSITY_HIGH, DisplayMetrics.DENSITY_TV};
+                } else {
+                    displayMetrics = new int[]{DisplayMetrics.DENSITY_XHIGH, DisplayMetrics.DENSITY_HIGH, DisplayMetrics.DENSITY_TV};
+                }
                 for (int displayMetric : displayMetrics) {
                     try {
                         Drawable d = otherAppCtx.getResources().getDrawableForDensity(pi.applicationInfo.icon, displayMetric);
@@ -150,14 +181,14 @@ public class StatusBarUtils {
                             return d;
                         }
                     } catch (Resources.NotFoundException e) {
-                        continue;
+                        e.printStackTrace();
                     }
                 }
             } catch (Exception e) {
                 // Handle Error here
             }
         }
-        ApplicationInfo appInfo = null;
+        ApplicationInfo appInfo;
         try {
             appInfo = pm.getApplicationInfo(packageName, 0);
         } catch (PackageManager.NameNotFoundException e) {
@@ -167,6 +198,18 @@ public class StatusBarUtils {
     }
 
 
+    public static int getIcon(Context context) {
+        return getIcon(context.getPackageName(), context);
+    }
+
+
+    /**
+     * 获得图标资源id
+     *
+     * @param packageName 哪怕，我们会
+     * @param context     上下文
+     * @return 图标
+     */
     public static int getIcon(String packageName, Context context) {
         PackageManager pm = context.getPackageManager();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
