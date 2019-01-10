6d26d21e03c1772297ad52875709e14a8f6670fb
WithWings <wangtong1175@sina.com>
2019-01-10 10:17:13 +0800

    删除无用导包

--- baseutils/src/main/java/com/withwings/baseutils/base/BaseSQLiteOpenHelper.java
+++ baseutils/src/main/java/com/withwings/baseutils/base/BaseSQLiteOpenHelper.java
@@ -4,7 +4,6 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.DatabaseErrorHandler;
-import android.database.DefaultDatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.NonNull;
